d5c1ec09149abba4a0eb5b461bd4cb994d2c38ba
leobaz <leutrim.kosumi@hotmail.com>
2024-04-19 14:13:29 +0200

    fix scroll to component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   standalone: true,
   imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.scss",
 })
 export class HeaderComponent {
+  constructor(private scroller: ViewportScroller) {}
 
+  onScrollToAnchor(component: string): void {
+    this.scroller.scrollToAnchor(component);
+  }
 }
